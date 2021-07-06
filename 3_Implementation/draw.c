129750364b543c2b5b350601a1ffc3d4fcc966c6
KCPooja <poojachandkc@gmail.com>
2021-07-06 19:40:44 +0530

    code for game

---
+++ 3_Implementation/draw.c
@@ -0,0 +1,37 @@
+//this to have the whole boundary of game
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <unistd.h>
+#include "fun.h"
+int i,j,score;
+int x,y,fx,fy;
+int draw()
+{
+    x=h/2;
+    y=w/2;
+    system("cls");
+    for(i=0;i<h;i++)
+    {
+        for(j=0;j<w;j++)
+        {
+            if(i==0||i==w-1||j==0||j==h-1)
+            {
+                printf("|");
+            }
+            else 
+            {
+                if(i==x && j==y)
+                    printf("^");
+                else if(i==fx && j==fy)
+                    printf("*");
+                else    
+                    printf(" ");
+            }   
+        }
+        printf("\n");
+    }
+    printf("Your Score:%d\n",score);
+    printf("Press Q to Quit the Game...\n");
+}
+

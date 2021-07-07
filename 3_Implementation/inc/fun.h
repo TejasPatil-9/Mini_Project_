c77cb8187e90f3ada01bd7709a07931c00eac681
KCPooja <poojachandkc@gmail.com>
2021-07-07 13:54:59 +0530

    header file update

--- 3_Implementation/inc/fun.h
+++ 3_Implementation/inc/fun.h
@@ -1,3 +1,13 @@
+/**
+ * @file fun.h
+ * @author K C Pooja (poojachandkc@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-07-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef __FUN_H
 #define __FUN_H
 extern int h, w, fx, fy;

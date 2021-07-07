cce05c4afae990f27c3f8c56e838517143838609
KCPooja <poojachandkc@gmail.com>
2021-07-07 13:58:01 +0530

    to give random locations

--- 3_Implementation/src/setup.c
+++ 3_Implementation/src/setup.c
@@ -1,3 +1,13 @@
+/**
+ * @file setup.c
+ * @author K C Pooja (poojachandkc@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-07-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 // to give fruit at different location
 #include "fun.h"
 #include <stdlib.h>

0b614a99ef436f3a04c11c6af78ba030a8da9e45
KCPooja <poojachandkc@gmail.com>
2021-07-07 13:55:42 +0530

    the whole snake game

--- 3_Implementation/snake_game.c
+++ 3_Implementation/snake_game.c
@@ -1,3 +1,13 @@
+/**
+ * @file snake_game.c
+ * @author K C Pooja (poojachandkc@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-07-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>

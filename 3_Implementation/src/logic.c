1e74a51d3cae82ef65de86a13a02d8ec6ebd46e2
KCPooja <poojachandkc@gmail.com>
2021-07-07 13:57:35 +0530

    logic for movement

--- 3_Implementation/src/logic.c
+++ 3_Implementation/src/logic.c
@@ -1,3 +1,13 @@
+/**
+ * @file logic.c
+ * @author K C Pooja (poojachandkc@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-07-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 //to know the movement and if it has crossed the boundary
 #include "fun.h"
 #include <stdio.h>

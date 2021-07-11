29177a435f8bde4be777bd9b5399ea754304a9a9
KCPooja <poojachandkc@gmail.com>
2021-07-11 18:13:48 +0530

    Test case file

---
+++ 3_Implementation/test/test_snakegame.c
@@ -0,0 +1,36 @@
+#include "unity.h"
+#include "../inc/fun.h"
+void setUp()
+{
+}
+void tearDown()
+{
+}
+
+void test_input(void)
+{  
+    TEST_PASS();
+}
+void test_logic(void)
+{
+    TEST_PASS();
+}
+void test_setup(void)
+{
+    TEST_PASS();
+}
+void test_draw(void)
+{
+    TEST_PASS();
+}
+#ifdef TESTING
+int main(void)
+{
+  UNITY_BEGIN();
+  RUN_TEST(test_input);
+  RUN_TEST(test_logic);
+  RUN_TEST(test_setup);
+  RUN_TEST(test_draw);
+  return UNITY_END();
+}
+#endif

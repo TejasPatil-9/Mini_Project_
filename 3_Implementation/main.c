898139d2f3f4d69583a807b0bbd159eb839e9afd
KCPooja <poojachandkc@gmail.com>
2021-07-12 01:14:27 +0530

    Update main

---
+++ 3_Implementation/main.c
@@ -0,0 +1,26 @@
+/**
+ * @file test.c
+ * @author K C Pooja (poojachandkc@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-07-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "../inc/fun.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <unistd.h>
+int gover=0;
+int main()
+{
+    setup();
+    while(!gover)
+    {
+        draw();
+        input();
+        logic();
+    }
+}
